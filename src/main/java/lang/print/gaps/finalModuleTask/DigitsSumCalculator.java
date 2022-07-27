ce0a0ea2cec65b2e492acdc57881bbf25a7a16a4
dilyor91 <dilyor_05@mail.ru>
2022-07-27 10:28:16 +0500

    FINAL MODULE TASK

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public static void calculateSum(int number) {
+        int sum = number/1000 + number/100%10 + number/10%10 + number%100%10;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    public static  void revert(int number) {
+        int reverse = (number%10)*100 + (number/10%10)*10 + number/100;
+        System.out.println(reverse);
     }
 }
