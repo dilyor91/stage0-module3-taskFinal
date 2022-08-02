c433d0fcb51590110dc448839832de5b20a71b5f
dilyor91 <dilyor_05@mail.ru>
2022-08-02 12:16:34 +0500

    FINAL MODULE TASK v2

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,10 +1,7 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
-    public void printIsPositive(int number) {
-        if(number > 0)
-            System.out.println(true);
-        else
-            System.out.println(false);
+    public static void printIsPositive(int number) {
+        System.out.println(number > 0);
     }
 }
